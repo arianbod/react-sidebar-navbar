8d5d6a365cee1e2de1039c0dc08c2148888f6511
Arian <Arianbod@outlook.com>
2023-12-31 12:31:05 +0300

    Sidebar logics added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import { UseTogglers } from './Togglers';
-
+import { UseTogglers } from './context';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import MenuItem from './MenuItem';
+import { links } from '../data';
+import Logo from '../logo.svg';
 const Sidebar = () => {
 	const { sidebarState, setSidebarState } = UseTogglers();
 	return (
 		<>
 			{sidebarState ? (
-				<div>
-					Sidebar
-					<button onClick={() => setSidebarState(!sidebarState)}>X</button>
+				<div className='sidebar'>
+					<section className='sidebar-header'>
+						<img src={Logo} />
+						<button
+							onClick={() => setSidebarState(!sidebarState)}
+							className='close-btn'>
+							<FaTimes />
+						</button>
+					</section>
+
+					<ul className='links'>
+						{links.map((item) => {
+							const { id, url, text, icon } = item;
+							return (
+								<li key={id}>
+									<a href={url}>
+										{icon}
+										<span className='Text'>{text}</span>
+									</a>
+								</li>
+							);
+						})}
+					</ul>
 				</div>
 			) : (
 				<button onClick={() => setSidebarState(!sidebarState)}>
-					Show Sidebar
+					<FaBars className='sidebar-toggler' />
 				</button>
 			)}
 		</>

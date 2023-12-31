3beed1c11e50768afa2b4db1eb9a92991edfc113
Arian <Arianbod@outlook.com>
2023-12-31 12:31:31 +0300

    AppContext name changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import AppContext from './components/Togglers';
+import AppContext from './components/context';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<AppContext>

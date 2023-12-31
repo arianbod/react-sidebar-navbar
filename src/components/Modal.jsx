20164282ebe82ae532c9cabafdedcce4115ffbd2
Arian <Arianbod@outlook.com>
2023-12-31 12:30:48 +0300

    conditional rendering logic updated to class base

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { UseTogglers } from './Togglers';
+import { UseTogglers } from './context';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Modal = () => {
 	const { modalState, setModalState } = UseTogglers();
 	return (
 		<>
-			{modalState ? (
-				<section>
-					Modal
-					<button onClick={() => setModalState(!modalState)}>x</button>
+			<div className='center-center'>
+				<section
+					className={modalState ? 'modal-overlay show-modal' : 'modal-overlay'}>
+					<section className='modal-container'>
+						<button
+							onClick={() => setModalState(!modalState)}
+							className='close-btn close-modal-btn'>
+							<FaTimes />
+						</button>
+						<h3>Modal Content</h3>
+					</section>
 				</section>
-			) : (
-				<button onClick={() => setModalState(!modalState)}>Show Modal</button>
-			)}
+				{!modalState && (
+					<button
+						onClick={() => setModalState(!modalState)}
+						className='btn'>
+						Show Modal
+					</button>
+				)}
+			</div>
 		</>
 	);
 };
